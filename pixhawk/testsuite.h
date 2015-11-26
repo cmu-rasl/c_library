e25a7501cc426b41a245c3037166a203c173afb8
Kumar Shaurya Shankar <kumarsha@cs.cmu.edu>
2015-11-26 12:39:28 -0800

    autogenerated headers for rev http://github.com/cmu-rasl/mavlink/tree/9d8ed429cec5860c5dc8230cde8d4bc69e38dab2

--- pixhawk/testsuite.h
+++ pixhawk/testsuite.h
@@ -972,6 +972,55 @@ static void mavlink_test_onboard_health(uint8_t system_id, uint8_t component_id,
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
+static void mavlink_test_l1_adaptive_debug(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+	mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+	mavlink_l1_adaptive_debug_t packet_in = {
+		93372036854775807ULL,{ 73.0, 74.0, 75.0 },{ 157.0, 158.0, 159.0 },{ 241.0, 242.0, 243.0 },{ 325.0, 326.0, 327.0 },{ 409.0, 410.0, 411.0 },20771
+    };
+	mavlink_l1_adaptive_debug_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        	packet1.timestamp = packet_in.timestamp;
+        	packet1.seq_id = packet_in.seq_id;
+        
+        	mav_array_memcpy(packet1.avl_hat, packet_in.avl_hat, sizeof(float)*3);
+        	mav_array_memcpy(packet1.dst_hat, packet_in.dst_hat, sizeof(float)*3);
+        	mav_array_memcpy(packet1.ang_vel, packet_in.ang_vel, sizeof(float)*3);
+        	mav_array_memcpy(packet1.lpd, packet_in.lpd, sizeof(float)*3);
+        	mav_array_memcpy(packet1.rates, packet_in.rates, sizeof(float)*3);
+        
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_l1_adaptive_debug_encode(system_id, component_id, &msg, &packet1);
+	mavlink_msg_l1_adaptive_debug_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_l1_adaptive_debug_pack(system_id, component_id, &msg , packet1.timestamp , packet1.seq_id , packet1.avl_hat , packet1.dst_hat , packet1.ang_vel , packet1.lpd , packet1.rates );
+	mavlink_msg_l1_adaptive_debug_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_l1_adaptive_debug_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.timestamp , packet1.seq_id , packet1.avl_hat , packet1.dst_hat , packet1.ang_vel , packet1.lpd , packet1.rates );
+	mavlink_msg_l1_adaptive_debug_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+        	comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+	mavlink_msg_l1_adaptive_debug_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_l1_adaptive_debug_send(MAVLINK_COMM_1 , packet1.timestamp , packet1.seq_id , packet1.avl_hat , packet1.dst_hat , packet1.ang_vel , packet1.lpd , packet1.rates );
+	mavlink_msg_l1_adaptive_debug_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
 static void mavlink_test_pixhawk(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 	mavlink_test_set_cam_shutter(system_id, component_id, last_msg);
@@ -993,6 +1042,7 @@ static void mavlink_test_pixhawk(uint8_t system_id, uint8_t component_id, mavlin
 	mavlink_test_attitude_control(system_id, component_id, last_msg);
 	mavlink_test_detection_stats(system_id, component_id, last_msg);
 	mavlink_test_onboard_health(system_id, component_id, last_msg);
+	mavlink_test_l1_adaptive_debug(system_id, component_id, last_msg);
 }
 
 #ifdef __cplusplus
